5fa633fa71c37c24a77ddfe6075bafb20e984c32
avotra <avotra@gmail.com>
2023-07-18 22:46:32 +0300

    exemple with image

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -13,46 +13,31 @@ class UploadController extends AbstractController
     #[Route('/', name: "app_upload_index")]
     public function index()
     {
-       
-
         return $this->render('/index.html.twig');
     }
 
-    #[Route('/video', name: "app_upload_video")]
-    public function video(Request $request,UploadService $uploadService)
-    {
-        $extensions = ['mp4','avi'];
-        if(!is_dir('videos')){
-            mkdir('videos');
-        }
-        $form = $this->createForm(UploadFormType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-           $uploadService->upload($extensions);
-        }  
-        return $this->render('/video.html.twig',['form'=>$form->createView()]);
-    }
-
     #[Route('/image', name: "app_upload_image")]
-    public function image(Request $request,UploadService $uploadService)
+    public function image(Request $request, UploadService $uploadService)
     {
-        $extensions = ['png','img'];
-        if(!is_dir('images')){
+        $extensions = ['png', 'img', 'jpg'];
+        if (!is_dir('images')) {
             mkdir('images');
         }
+        $fichier = $uploadService->getAll('images');
         $form = $this->createForm(UploadFormType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $uploadService->upload($extensions);
-        }  
-        return $this->render('/image.html.twig',['form'=>$form->createView()]);
+        }
+
+        return $this->render('/image.html.twig', ['form' => $form->createView(), 'fichiers' => $fichier]);
     }
-    #[Route('/image/new', name: "app_new_image")]
-    public function imageNew()
+    
+    #[Route('/video', name: "app_upload_video")]
+    public function video()
     {
-        
+        return $this->render('/video.html.twig');
     }
 
     #[Route('/document', name: "app_upload_doc")]
--- src/Service/UploadService.php
+++ src/Service/UploadService.php
@@ -1,17 +1,29 @@
 <?php
 
-    namespace App\Service;
+namespace App\Service;
 
-    class UploadService
+class UploadService
+{
+
+    public function upload($extensions)
     {
-        public function upload($extensions){
-            $nomFichier = $_FILES['upload_form']['name']['file'];
-            $pointPosition = strpos($nomFichier,'.');
-            $extension = substr($nomFichier, $pointPosition+1);
-            $extension = strtolower($extension);
-            $nouveauFichier = 'images/'.uniqid().'.'.$extension;
-            if(in_array($extension,$extensions)){
-                move_uploaded_file($_FILES['upload_form']['tmp_name']['file'],$nouveauFichier);
-            }
+        $nomFichier = $_FILES['upload_form']['name']['file'];
+        $pointPosition = strpos($nomFichier, '.');
+        $extension = substr($nomFichier, $pointPosition + 1);
+        $extension = strtolower($extension);
+        $cle = uniqid();
+        if (in_array($extension, $extensions)) {
+            move_uploaded_file($_FILES['upload_form']['tmp_name']['file'], 'images/' . $cle . '.' . $extension);
         }
     }
+    public function getAll($chemin)
+    {
+        if (is_dir($chemin)) {
+            $fichiers = scandir($chemin);
+            $fichiers = array_filter($fichiers, function ($fichier) {
+                return !in_array($fichier, ['.', '..']);
+            });
+        }
+        return $fichiers;
+    }
+}
